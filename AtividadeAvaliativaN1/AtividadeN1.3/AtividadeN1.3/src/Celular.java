715539d713376b523760341fdf448b4dd59ab491
albertovisentainer <albertovisentainer@icloud.com>
2025-10-28 21:33:50 -0300

    atividade 3 completa aula poo

--- AtividadeAvaliativaN1/AtividadeN1.3/AtividadeN1.3/src/Celular.java
+++ AtividadeAvaliativaN1/AtividadeN1.3/AtividadeN1.3/src/Celular.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
-
 public class Celular {
+    // atributos para celular
+
+    private String marca;
+    private String modelo;
+    private Integer capacidadeGB;
+    private boolean estado;
+
+    // construtor para molde de celular
+    public Celular(String marca, String modelo, Integer capacidadeGB, boolean estado) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.capacidadeGB = capacidadeGB;
+        this.estado = estado;
+    }
+
+
+    // get e set para buscar ou alterar atributos
+
+    public String getMarca(){
+        return marca;
+    }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public Integer getCapacidadeGB(){
+        return capacidadeGB;
+    }
+    public void setCapacidadeGB(Integer capacidadeGB){
+        this.capacidadeGB = capacidadeGB;
+    }
+    public Boolean getEstado(){
+        return estado;
+    }
+    public void setEstado(boolean estado){
+        this.estado = estado;
+    }
+
+    // metodos para mudar o estado do celular ligado/desligado
+
+    public void ligar(){
+        if (!estado){
+            estado = true;
+            System.out.println(modelo + " Ligado!");
+        } else {
+            System.out.println(modelo + " Já está ligado!");
+        }
+    }
+    public void desligar(){
+        if (estado){
+            estado = false;
+            System.out.println(modelo + " Desligado!");
+        }else {
+            System.out.println(modelo + " Já está desligado!");
+        }
+    }
 }
--- AtividadeAvaliativaN1/AtividadeN1.3/AtividadeN1.3/src/Main.java
+++ AtividadeAvaliativaN1/AtividadeN1.3/AtividadeN1.3/src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // criar o objeto
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Celular celular1 = new Celular("Apple", "iPhone 14 Pro Max", 256, true);
+        Celular celular2 = new Celular("Apple", "iPhone 13", 128, false);
+        Celular celular3 = new Celular("Motorola", "Moto G54", 128, false);
+
+
+        System.out.println("Marca: " + celular3.getMarca());
+        System.out.println("Modelo " + celular3.getModelo());
+
+
+        // uso do set para alterar o nome da marca e modelo do celular3
+        celular3.setMarca("Samsung");
+        celular3.setModelo("S24 Fe");
+
+        // exibição da nova marca do Celular3 com o get depois de ser alterado com o uso do set
+        System.out.println("\nMarca do Celular3 alterado para: " + celular3.getMarca());
+        System.out.println("Modelo do Celular3 alterado para: " + celular3.getModelo());
+
+
+        // exibir as caracteristicas do celular com o uso do get
+        System.out.println("\nCelular disponível: ");
+        System.out.println("Marca " + celular3.getMarca());
+        System.out.println("Modelo: " + celular3.getModelo());
+        System.out.println("Capacidade de armazenamento: " + celular3.getCapacidadeGB() + "GB");
+
+        // métodos de manipulação do objeto celular
+        celular3.ligar();
+
     }
 }
